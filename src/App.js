6a282797c2470f123dd699df47224738189ce01c
bastien <coetabastienz@gmail.com>
2024-04-20 16:49:59 +0200

    zss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom" 
+import Home from './page/home';
+import Signin from "./page/signin";
+import Signup from "./page/signup";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Header />
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />}/>
+          <Route path="/signin" element={<Signin />}/>
+          <Route path="*" element={<p>Error 404</p>} />
+      </Routes>
+      <Footer />
+      <ToastContainer
+            position="top-center"
+            autoClose={1000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+    </Router>
+  
+    </>
   );
 }
 
---
+++ src/page/home/index.jsx
@@ -0,0 +1,10 @@
+import Post from "../../components/Post"
+const Home = () => {
+  return (
+    <div>
+      <Post />
+    </div>
+  )
+}
+
+export default Home
